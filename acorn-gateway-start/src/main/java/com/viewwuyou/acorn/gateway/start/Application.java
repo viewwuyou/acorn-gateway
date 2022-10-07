af3e90581253c4e2171ea4c0befc1ad00267187e
viewwuyou <viewwuyou@gmail.com>
2022-10-07 14:22:32 +0800

    init project

--- acorn-gateway-start/src/main/java/com/viewwuyou/acorn/gateway/start/Application.java
+++ acorn-gateway-start/src/main/java/com/viewwuyou/acorn/gateway/start/Application.java
@@ -1,7 +1,5 @@
 package com.viewwuyou.acorn.gateway.start;
 
-import com.viewwuyou.acorn.gateway.domain.DomainServiceCenter;
-import com.viewwuyou.acorn.gateway.domain.config.ConfigDomainService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -13,10 +11,5 @@ import org.springframework.context.ApplicationContext;
 public class Application {
     public static void main(String[] args) {
         ApplicationContext applicationContext = SpringApplication.run(Application.class, args);
-        DomainServiceCenter domainServiceCenter = applicationContext.getBean(DomainServiceCenter.class);
-        ConfigDomainService configDomainService = domainServiceCenter.getDomainService(ConfigDomainService.class);
-        if (configDomainService != null) {
-            System.out.println(configDomainService.getDomainName() + " " + configDomainService.getImplCode());
-        }
     }
 }
