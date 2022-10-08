5ea477a770fa984b0d7cb05bc3fd2c51ea076fbc
viewwuyou <viewwuyou@gmail.com>
2022-10-08 16:34:05 +0800

    init project

--- acorn-gateway-start/src/main/java/com/viewwuyou/acorn/gateway/start/Application.java
+++ acorn-gateway-start/src/main/java/com/viewwuyou/acorn/gateway/start/Application.java
@@ -11,6 +11,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        System.out.println(DruidStatManagerFacade.getInstance().getDataSourceStatDataList());
     }
 }
